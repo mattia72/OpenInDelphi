c66f1e3742a2da957cae644ccc0bbf6bf59b2173
mattia72 <mattiassicha@gmail.com>
2025-07-23 08:10:26 +0200

    delete not used requires

--- extension.js
+++ extension.js
@@ -1,8 +1,5 @@
 const vscode = require('vscode');
-const fs = require('fs');
 const { DelphiCommander } = require('./delphiCommander');
-const path = require('path');
-const os = require('os');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,7 +8,7 @@ const os = require('os');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "openindelphi" is now active!');
+	console.log('Congratulations, your extension "OpenInDelphi" is now active!');
 	const disposable = vscode.commands.registerCommand('openindelphi.openCurrentFileInDelphi', DelphiCommander.openCurrentFileInDelphi);
 	context.subscriptions.push(disposable);
 }
